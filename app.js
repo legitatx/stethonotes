370e34e20fd3c989ff8600db67fb5c88f0de6fdc
LegitModern <legitmodern.dev@gmail.com>
2017-10-29 05:13:58 -0500

    CORS middleware.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const winston = require('winston');
@@ -14,6 +15,8 @@ const logger = winston.createLogger({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cors());
 
 app.use((req, res, next) => {
   res.logger = logger;
