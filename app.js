b980ee59ca7213bfac3a273fc77580f11e341973
LegitModern <legitmodern.dev@gmail.com>
2017-10-29 06:41:09 -0500

    Test

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const logger = winston.createLogger({
   ]
 });
 
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(cors());
 
 app.use((req, res, next) => {
   res.logger = logger;
