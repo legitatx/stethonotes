4c4b649c8fedd7bf257a51ca6eefad6a53fb8ed8
LegitModern <legitmodern.dev@gmail.com>
2017-10-28 23:35:16 -0500

    Boilerplate API in Flask.

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import patient from './patientReducer';
-
-export default combineReducers({
-  patient,
-  form: formReducer
-});
