7fc1e57b49eed8c02c8b55fab2b0e07b136eb4c3
Lawrence Khadka <LKhadka@users.noreply.github.com>
2017-10-29 04:35:14 -0500

    add quote

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,13 @@ const About = () => (
     <h1 style={{ color: 'rgba(0,0,0,0.5)' }}>
       Medical Transcriptions Made Simple
     </h1>
+    <blockquote>
+      "EHRs were implemented to improve the efficiency and quality of patient
+      care, but they have yet to achieve that promise,"
+    </blockquote>
+    <h4 style={{ color: 'rgba(0,0,0,0.8)', marginTop: '0.2em' }}>
+      Susan Hingle, MD
+    </h4>
   </div>
 );
 
