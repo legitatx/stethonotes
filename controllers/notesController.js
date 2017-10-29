c389e78bfbe68d12f1df2ccf186b1814ec5b56d0
LegitModern <legitmodern.dev@gmail.com>
2017-10-29 07:17:02 -0500

    Switich to file uploading.

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -2,23 +2,32 @@ require('dotenv').config({path: 'variables.env'});
 const SpeechService = require('ms-bing-speech-service');
 const fs = require('fs');
 const crypto = require('crypto');
+const formidable = require('formidable');
+const path = require('path');
 
 const recognition = new SpeechService({language: 'en-US', subscriptionKey: process.env.BING_SPEECH_KEY});
 
 exports.init = (req, res) => {
-  res.json({'status': 200})
+  res.json({'status': 200});
 };
 
 exports.sendRecording = (req, res) => {
-  const buffer = new Buffer(`${req.body.blob}`, 'base64');
-  const token = crypto.randomBytes(64).toString('hex');
-  fs.writeFile(`temp/recording-${token}.wav`, buffer, (error) => {
+  const form = new formidable.IncomingForm()
+  form.multiples = false;
+  form.keepExtensions = true;
+  form.uploadDir = '/uploads';
+  form.parse(req, (error, fields, files) => {
     if (error) {
       res.json({'status': 500, 'error': error})
-      res.logger.error(`An error occured while attempting to decode a blob: ${error}`);
+      res.logger.error(`An error occured while attempting to upload a file: ${error}`);
     } else {
-      res.json({'status': 204})
-      res.logger.info('Successfully wrote file!')
+      res.json({'status': 204});
+      res.logger.info('Successfully uploaded file!');
     }
   });
-}
+  form.on('fileBegin', (name, file) => {
+    const [name, extension] = file.name.split('.')
+    const token = crypto.randomBytes(16).toString('hex');
+    file.path = path.join(uploadDir, `${name}-${token}.${extension}`)
+  })
+};
