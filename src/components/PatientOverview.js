2a640dd25dc447fef2869b9cad95dffecc1d4277
Lawrence Khadka <LKhadka@users.noreply.github.com>
2017-10-29 11:15:29 -0500

    add patient overview

--- src/components/PatientOverview.js
+++ src/components/PatientOverview.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
-import TextField from 'material-ui/TextField';
 import RefreshIndicator from 'material-ui/RefreshIndicator';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
+import Checkbox from 'material-ui/Checkbox';
+import PlusOne from 'material-ui/svg-icons/image/exposure-plus-1';
+import Header from './Header';
+
+const data = {
+  locations: [
+    { id: 1, name: 'Frontal Right', checked: true },
+    { id: 2, name: 'Option 2' },
+    { id: 3, name: 'Option 3' }
+  ],
+  radiations: [
+    { id: 1, name: 'Neck', checked: true },
+    { id: 2, name: 'Shoulders' },
+    { id: 3, name: 'Upper thorax' }
+  ],
+  qualities: [
+    { id: 1, name: 'Sharp', checked: true },
+    { id: 2, name: 'Squeezing', checked: true }
+  ]
+};
 
 class PatientOverview extends Component {
   state = {
@@ -44,7 +64,73 @@ class PatientOverview extends Component {
         </div>
       );
     }
-    return <div>Hiya</div>;
+    return (
+      <div>
+        <Header hideNav={false} toggleDrawer={this.toggleDrawer} />
+
+        <h2 style={{ textAlign: 'center' }}>Patient Overview</h2>
+        <Card>
+          <CardHeader title='Locations' />
+          <CardText style={{ paddingTop: '.1rem' }}>
+            {data.locations.map(
+              location =>
+                location.checked && (
+                  <Checkbox
+                    key={location.id}
+                    label={location.name}
+                    checked={location.checked}
+                  />
+                )
+            )}
+            <Checkbox
+              label='Add'
+              uncheckedIcon={<PlusOne />}
+              style={{ marginTop: '.5rem' }}
+            />
+          </CardText>
+        </Card>
+        <Card>
+          <CardHeader title='Radiations' />
+          <CardText style={{ paddingTop: '.1rem' }}>
+            {data.radiations.map(
+              radiation =>
+                radiation.checked && (
+                  <Checkbox
+                    key={radiation.id}
+                    label={radiation.name}
+                    checked={radiation.checked}
+                  />
+                )
+            )}
+            <Checkbox
+              label='Add'
+              uncheckedIcon={<PlusOne />}
+              style={{ marginTop: '.5rem' }}
+            />
+          </CardText>
+        </Card>
+        <Card>
+          <CardHeader title='Qualities' />
+          <CardText style={{ paddingTop: '.1rem' }}>
+            {data.qualities.map(
+              quality =>
+                quality.checked && (
+                  <Checkbox
+                    key={quality.id}
+                    label={quality.name}
+                    checked={quality.checked}
+                  />
+                )
+            )}
+            <Checkbox
+              label='Add'
+              uncheckedIcon={<PlusOne />}
+              style={{ marginTop: '.5rem' }}
+            />
+          </CardText>
+        </Card>
+      </div>
+    );
   }
 }
 
